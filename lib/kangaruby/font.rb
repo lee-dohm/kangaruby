6bcb433f588154e1a5c810bdb752b009f7c8e708
Lee Dohm <lee@liftedstudios.com>
2013-04-20 07:51:45 -0700

    Add ability to determine width and height of text strings

--- lib/kangaruby/font.rb
+++ lib/kangaruby/font.rb
@@ -5,6 +5,10 @@
 require 'ttfunk'
 
 module KangaRuby
+  # Represents error that occurs when a font does not contain a Unicode map.
+  class MissingUnicodeMapError < StandardError
+  end
+
   # Embodies a TrueType font, allowing for calculation of visual sizes of strings.
   class Font
     # Opens the given font.
@@ -12,6 +16,7 @@ module KangaRuby
     # @param [String] path Path to the font.
     def initialize(path)
       @font = TTFunk::File.open(path)
+      @char_widths = []
     end
 
     # Gets the name of the font.
@@ -26,6 +31,52 @@ module KangaRuby
     # @return [Float] Scale factor for the font.
     def scale_factor
       @scale ||= 1000.0 / @font.header.units_per_em
+    end
+
+    # Height of text for the font.
+    def text_height
+      ascender - descender + line_gap
+    end
+
+    # Gets the width of the given string of text.
+    #
+    # @param [String] text Text to get the width for.
+    def text_width(text)
+      base_width = text.codepoints.reduce(0) do |width, code|
+        width + character_width(code)
+      end
+
+      base_width * scale_factor
+    end
+
+    private
+
+    def ascender
+      @ascender ||= @font.ascent * scale_factor
+    end
+
+    def character_width(code)
+      return 0 unless cmap[code]
+
+      return 0.0 if code == 10
+
+      @char_widths[code] ||= Integer(horizontal_metrics.widths[cmap[code]] * scale_factor)
+    end
+
+    def cmap
+      @cmap ||= @font.cmap.unicode.first or raise MissingUnicodeMapError
+    end
+
+    def descender
+      @descender ||= @font.descent * scale_factor
+    end
+
+    def horizontal_metrics
+      @horizontal_metrics ||= @font.horizontal_metrics
+    end
+
+    def line_gap
+      @line_gap ||= @font.line_gap * scale_factor
     end
   end
 end
--- spec/font_spec.rb
+++ spec/font_spec.rb
@@ -4,6 +4,7 @@
 
 describe Font do
   let(:font) { Font.new './fonts/Abscissa.ttf' }
+  let(:text) { 'The quick brown fox jumped over the lazy dog' }
 
   it 'will have a name' do
     expect(font.name).to eq('Abscissa')
@@ -13,5 +14,8 @@ describe Font do
     expect(font.scale_factor).to eq(1.0)
   end
 
-  it 'can calculate the width and height of a given string of text'
+  it 'can calculate the width and height of a given string of text' do
+    expect(font.text_width(text)).to eq(18679.0)
+    expect(font.text_height).to eq(1352.0)
+  end
 end
