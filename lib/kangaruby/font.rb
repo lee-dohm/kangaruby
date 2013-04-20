b7d3a784f58aa4609bcd4abeb598b530b74d783c
Lee Dohm <lee@liftedstudios.com>
2013-04-20 08:11:56 -0700

    Add more documentation to the Font class

--- lib/kangaruby/font.rb
+++ lib/kangaruby/font.rb
@@ -10,6 +10,10 @@ module KangaRuby
   end
 
   # Embodies a TrueType font, allowing for calculation of visual sizes of strings.
+  #
+  # These calculations are documented, for the most part, on the
+  # [FreeType Glyph Conventions](http://freetype.sourceforge.net/freetype2/docs/glyphs/index.html)
+  # page.
   class Font
     # Opens the given font.
     #
@@ -51,10 +55,17 @@ module KangaRuby
 
     private
 
+    # Gets the scaled ascender value.
+    #
+    # @return [Float] The distance from the baseline to the highest point of a character's glyph.
     def ascender
       @ascender ||= @font.ascent * scale_factor
     end
 
+    # Gets the scaled width for a given character code.
+    #
+    # @param [Fixnum] code Number representing a UTF-8 character code.
+    # @return [Float] The advance width of the given character.
     def character_width(code)
       return 0 unless cmap[code]
 
@@ -63,18 +74,30 @@ module KangaRuby
       @char_widths[code] ||= Integer(horizontal_metrics.widths[cmap[code]] * scale_factor)
     end
 
+    # Gets the Unicode character map for the font.
+    #
+    # @return [Array] Unicode character map.
     def cmap
       @cmap ||= @font.cmap.unicode.first or raise MissingUnicodeMapError
     end
 
+    # Gets the scaled descender value.
+    #
+    # @return [Float] The distance from the baseline to the lowest point of a character's glyph.
     def descender
       @descender ||= @font.descent * scale_factor
     end
 
+    # Gets the horizontal metrics table for the font.
+    #
+    # @return [Array] Table of horizontal metrics.
     def horizontal_metrics
       @horizontal_metrics ||= @font.horizontal_metrics
     end
 
+    # Gets the scaled line gap.
+    #
+    # @return [Float] The distance that must be placed between two lines of text.
     def line_gap
       @line_gap ||= @font.line_gap * scale_factor
     end
