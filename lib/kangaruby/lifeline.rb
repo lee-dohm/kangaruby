7cbadad27c44c04cd0e2d0c5fd07377b605ba043
Lee Dohm <lee@liftedstudios.com>
2013-04-22 21:41:31 -0700

    Flesh out the Lifeline class

--- lib/kangaruby/lifeline.rb
+++ lib/kangaruby/lifeline.rb
@@ -5,5 +5,36 @@
 module KangaRuby
   # Represents the lifetime of a participant in the diagram.
   class Lifeline
+    # Reference to the head symbol.
+    attr_reader :head
+
+    # Reference to the tail symbol.
+    attr_reader :tail
+
+    # Creates a new lifeline.
+    #
+    # @param [String] name Name for the lifeline.
+    # @param [Font] font Font object to use to draw the name.
+    def initialize(name, font)
+      @head = LifelineHead.new(name, font)
+      @tail = LifelineTail.new
+    end
+
+    # Gets the minimum size of the lifeline in pixels.
+    #
+    # @return [Array] Minimum size width and height in pixels.
+    def minimum_size
+      w = [@head.minimum_size[0], @tail.minimum_size[0]].max
+      h = @head.minimum_size[1] + @tail.minimum_size[1]
+
+      [w, h]
+    end
+
+    # Gets the name for the lifeline.
+    #
+    # @return [String] Name of the lifeline.
+    def name
+      @head.name
+    end
   end
 end
--- spec/lifeline_spec.rb
+++ spec/lifeline_spec.rb
@@ -3,13 +3,34 @@
 #
 
 describe Lifeline do
-  it 'has a name'
+  let(:font) { Font.new('fonts/Abscissa.ttf') }
+  let(:lifeline) { Lifeline.new('Alice', font) }
 
-  it 'has a head'
+  it 'has a name' do
+    expect(lifeline.name).to eq('Alice')
+  end
 
-  it 'has a tail'
+  it 'has a head' do
+    expect(lifeline.head).to_not be_nil
+    expect(lifeline.head.name).to eq('Alice')
+    expect(lifeline.head.font).to be(font)
+  end
 
-  it 'can describe its minimum size'
+  it 'has a tail' do
+    expect(lifeline.tail).to_not be_nil
+  end
+
+  it 'can describe its minimum size' do
+    head_width = lifeline.head.minimum_size[0]
+    tail_width = lifeline.tail.minimum_size[0]
+    expected_width = [head_width, tail_width].max
+
+    head_height = lifeline.head.minimum_size[1]
+    tail_height = lifeline.tail.minimum_size[1]
+    expected_height = head_height + tail_height
+
+    expect(lifeline.minimum_size).to eq([expected_width, expected_height])
+  end
 
   it 'can draw itself'
 end
