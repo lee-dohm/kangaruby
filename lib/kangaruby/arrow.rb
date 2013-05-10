53c6b0b9f6a3f33fa9860c8fa5baa8b85ee3ccb7
Lee Dohm <lee@liftedstudios.com>
2013-05-09 20:36:22 -0700

    Add ability for arrow to draw itself

--- lib/kangaruby/arrow.rb
+++ lib/kangaruby/arrow.rb
@@ -5,6 +5,8 @@
 module KangaRuby
   # Represents an action in the diagram.
   class Arrow
+    include GraphicsUtilities
+
     # @return [Symbol] Line style to use for drawing the arrow.
     attr_reader :style
 
@@ -28,7 +30,17 @@ module KangaRuby
     def draw(rect, doc)
       g = doc.create_element 'g', stroke: 'black', 'stroke-width' => '1'
 
+      add_child(g, 'line', x1: rect.right - 5, y1: center_y(rect) - 5, x2: rect.right, y2: center_y(rect))
+      add_child(g, 'line', x1: rect.right - 5, y1: center_y(rect) + 5, x2: rect.right, y2: center_y(rect))
+      add_child(g, 'line', x1: rect.left, y1: center_y(rect), x2: rect.right, y2: center_y(rect))
+
       g
+    end
+
+    private
+
+    def add_child(element, *args)
+      element.add_child(element.document.create_element(*args))
     end
   end
 end
--- spec/arrow_spec.rb
+++ spec/arrow_spec.rb
@@ -4,6 +4,18 @@
 
 describe Arrow do
   let(:arrow) { Arrow.new }
+  let(:doc) { Nokogiri::XML::Document.new }
+  let(:rect) { Rect.new [100] * 4 }
+
+  let(:drawn_node) do
+    create_node <<-EOS
+<g stroke='black' stroke-width='1'>
+  <line x1='195' y1='145' x2='200' y2='150' />
+  <line x1='195' y1='155' x2='200' y2='150' />
+  <line x1='100' y1='150' x2='200' y2='150' />
+</g>
+    EOS
+  end
 
   it 'has a line style' do
     expect(arrow.style).to eq(:solid)
@@ -20,5 +32,9 @@ describe Arrow do
     expect(arrow.minimum_size).to eq([10, 10])
   end
 
-  it 'can draw itself'
+  it 'can draw itself' do
+    node = arrow.draw(rect, doc)
+
+    expect(node).to be_equivalent_to(drawn_node)
+  end
 end
