54a1c4f1eb967e7de2ef36f8810bfb17101d992e
Lee Dohm <lee@liftedstudios.com>
2013-05-11 09:45:20 -0700

    Take out the blank_diagram because it was stupid

--- spec/diagram_spec.rb
+++ spec/diagram_spec.rb
@@ -3,14 +3,14 @@
 #
 
 describe Diagram do
-  let(:blank_diagram) { Diagram.new }
+  let(:diagram) { Diagram.new }
 
   it 'has a collection of lifelines' do
-    expect(blank_diagram.lifelines).to eq([])
+    expect(diagram.lifelines).to eq([])
   end
 
   it 'has a collection of arrows' do
-    expect(blank_diagram.arrows).to eq([])
+    expect(diagram.arrows).to eq([])
   end
 
   it 'can draw itself'
