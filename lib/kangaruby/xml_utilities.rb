a638175502e9253b464a5fe4ea72705109f8387b
Lee Dohm <lee@liftedstudios.com>
2013-05-11 12:09:26 -0700

    Return the element that was added to allow better chaining

--- lib/kangaruby/xml_utilities.rb
+++ lib/kangaruby/xml_utilities.rb
@@ -9,11 +9,9 @@ module KangaRuby
     #
     # @param [Nokogiri::XML::Element] element Element to which to add the child.
     # @param [Array<String, Hash>] args Node name followed by attributes of the node in a Hash.
-    # @return [nil]
+    # @return [Nokogiri::XML::Element] Element that was added.
     def add_child(element, *args)
       element.add_child(element.document.create_element(*args))
-
-      nil
     end
   end
 end
