e2f18683360c621736c8918ffb5bc6a83198dd04
Lee Dohm <lee@liftedstudios.com>
2013-05-19 08:09:12 -0700

    Add pointer to fonts directory and constants spec

--- lib/kangaruby.rb
+++ lib/kangaruby.rb
@@ -25,6 +25,9 @@ require 'kangaruby/version'
 require 'kangaruby/xml_utilities'
 
 module KangaRuby
+  # Directory containing the fonts for the gem.
+  FONTS_DIR = File.expand_path('../../fonts', __FILE__)
+
   # Acceptable list of line styles.
   STYLES = [:solid, :dotted]
 end
---
+++ spec/constants_spec.rb
@@ -0,0 +1,12 @@
+
+describe 'Constants' do
+  it 'has a pointer to the fonts directory' do
+    expect(KangaRuby::FONTS_DIR).to eq(File.expand_path('../../fonts', __FILE__))
+  end
+
+  it 'supplies acceptable line styles' do
+    expect(KangaRuby::STYLES.count).to eq(2)
+    expect(KangaRuby::STYLES).to include(:solid)
+    expect(KangaRuby::STYLES).to include(:dotted)
+  end
+end
