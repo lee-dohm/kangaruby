d485af30b9b642e972110a82a7f05b87daaa52a4
Lee Dohm <lee@liftedstudios.com>
2013-06-01 12:02:58 -0700

    Add properties to the arrow factories

--- spec/factories/svg.rb
+++ spec/factories/svg.rb
@@ -6,10 +6,15 @@ FactoryGirl.define do
   factory :arrow_left, class: Nokogiri::XML::Document do
     skip_create
 
+    ignore do
+      stroke 'black'
+      width 1
+    end
+
     initialize_with do
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.svg(xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: '1000', height: '1000') do
-          xml.g(stroke: 'black', 'stroke-width' => '1') do
+          xml.g(stroke: stroke, 'stroke-width' => width) do
             xml.line(x1: '105', y1: '145', x2: '100', y2: '150')
             xml.line(x1: '105', y1: '155', x2: '100', y2: '150')
             xml.line(x1: '200', y1: '150', x2: '100', y2: '150')
@@ -24,10 +29,15 @@ FactoryGirl.define do
   factory :arrow_right, class: Nokogiri::XML::Document do
     skip_create
 
+    ignore do
+      stroke 'black'
+      width 1
+    end
+
     initialize_with do
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.svg(xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: '1000', height: '1000') do
-          xml.g(stroke: 'black', 'stroke-width' => '1') do
+          xml.g(stroke: stroke, 'stroke-width' => width) do
             xml.line(x1: '195', y1: '145', x2: '200', y2: '150')
             xml.line(x1: '195', y1: '155', x2: '200', y2: '150')
             xml.line(x1: '100', y1: '150', x2: '200', y2: '150')
